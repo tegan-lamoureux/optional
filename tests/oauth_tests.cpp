bc25c5640f9c80b0cfeddc348bada4487217b11e
Tegan Lamoureux <tegan@teux.me>
2019-02-23 19:17:34 -0800

    Update oauth_tests.cpp

--- tests/oauth_tests.cpp
+++ tests/oauth_tests.cpp
@@ -4,6 +4,8 @@
 #include <curl/curl.h>
 #include <string>
 
+using namespace std;
+
 namespace {
 
 TEST(OAuthTests, curl_exists_and_can_be_used) {
@@ -15,7 +17,27 @@ TEST(OAuthTests, can_create_oauth_object) {
 }
 
 TEST(OAuthTests, generate_authentication_url_composes_correct_string) {
+    Optional::OAuth oauth("user_id", "https://callback_uri.test");
+    const string expected_url = "https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=https://callback_uri.test&client_id=user_id@AMER.OAUTHAP";
+    
+    EXPECT_STREQ(expected_url, oauth.generate_authentication_url());
+}
+    
+TEST(OAuthTests, oauth_handles_bad_uid_and_url) {
+    Optional::OAuth oauth("", "");
+    const string expected_url = "https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=https://null_value&client_id=null_value@AMER.OAUTHAP";
 
+    EXPECT_STREQ(expected_url, oauth.generate_authentication_url());
+}
+    
+TEST(OAuthTests, accept_authentication_code_accepts_good_code) {
+    Optional::OAuth oauth("user_id", "https://callback_uri.test");
+    
+    const string provided_code = "http://callback_uri.test/?code=<THIS_IS_THE_RETURNED_AUTH_CODE>";
+    const string expected_code = "<THIS_IS_THE_RETURNED_AUTH_CODE>";
+    
+    EXPECT_TRUE(oauth.accept_authentication_code(provided_code));
+    EXPECT_STREQ(expected_code, oauth.get_authentication_code());
 }
 
 } // namespace
