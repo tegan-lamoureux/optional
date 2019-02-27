f84c3861587d7ee924c223700c364f3050028ddb
Tegan Lamoureux <tegan@teux.me>
2019-02-27 14:11:35 -0800

    OAuth authentication works!!!

--- src/backend/oauth.cpp
+++ src/backend/oauth.cpp
@@ -1,5 +1,6 @@
 #include "oauth.h"
 #include <curl/curl.h>
+#include <cstring>
 
 // FIXME: Change these namespaces to split the frontend and backend? Maybe. Check.
 
@@ -22,8 +23,8 @@ Optional::OAuth::OAuth(std::string oauth_uid_in, std::string redirect_uri_in)
 }
 
 Optional::OAuth::~OAuth() {
-    /* always cleanup */ 
-    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+
 }
 
 std::string Optional::OAuth::generate_authentication_url() {
@@ -50,33 +51,69 @@ bool Optional::OAuth::accept_authentication_code(std::string code) {
     return valid;
 }
 
-std::string Optional::get_authentication_code() {
+std::string Optional::OAuth::get_authentication_code() {
     return this->authentication_code;
 }
 
 std::string Optional::OAuth::generate_refresh_token() {
-    // left off here. post to this->access_token_post_url with the given
-    // authentication code and user info. same for access token.
-	
     CURL *curl = curl_easy_init();
     CURLcode res;
 	
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, this->access_token_post_url);
-	
-        /* Now specify the POST data */ 
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "field1=datahere&field2=moredata"); // FIXME with real data
+    if(curl && this->authentication_code.empty() == false) { // FIXME: Change to enum unauthenticated.
+        struct WriteThis wt;
+        struct weird_string ws;
+
+        std::string post_data = std::string("grant_type=authorization_code")
+                + std::string("&refresh_token=")
+                + std::string("&access_type=offline")
+                + std::string("&code=")
+                + this->authentication_code
+                + std::string("&client_id=")
+                + this->oauth_uid
+                + std::string("&redirect_uri=")
+                + this->redirect_uri;
+
+        wt.readptr = post_data.c_str();
+        wt.sizeleft = post_data.length();
+
+        curl_easy_setopt(curl, CURLOPT_URL, this->access_token_post_url.c_str());
+        curl_easy_setopt(curl, CURLOPT_POST, 1L);
+        curl_easy_setopt(curl, CURLOPT_READFUNCTION, this->read_callback);
+        curl_easy_setopt(curl, CURLOPT_READDATA, &wt);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ws);
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long)wt.sizeleft);
+
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK) {
+          fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+        else {
+            fprintf(stderr, "success! : %s\n");
+        }
+
+        /* Now specify the POST data */
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data.c_str()); // FIXME with real data
  
-        /* Perform the request, res will get the return code */ 
+        /* Perform the request, res will get the return code */
         res = curl_easy_perform(curl);
 	    
-        /* Check for errors */ 
+        /* Check for errors */
         if(res != CURLE_OK) {
-            //fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res)); // FIXME: err here, probs with status enum
-	}
-	    
-	// TODO: Figure out how to get resulting code back? Does a post return anything? Or do I need to do a corresponding get? HOW DOES THIS WORK
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+        else {
+            fprintf(stderr, "OMG IT WORKED\n", curl_easy_strerror(res));
+            fprintf(stderr, "data: %s\n", ws.ptr);
+        }
+
+        // TODO: Figure out how to get resulting code back? Does a post return anything? Or do I need to do a corresponding get? HOW DOES THIS WORK
+
+        curl_easy_cleanup(curl);
     }
+
+    return this->refresh_token;
 }
 
 std::string Optional::OAuth::generate_access_token() {
@@ -86,4 +123,39 @@ std::string Optional::OAuth::generate_access_token() {
 std::string Optional::OAuth::debug()
 {
     return this->authentication_code;
+}
+
+size_t Optional::OAuth::read_callback(void *dest, size_t size, size_t nmemb, void *userp)
+{
+  struct WriteThis *wt = (struct WriteThis *)userp;
+  size_t buffer_size = size*nmemb;
+
+  if(wt->sizeleft) {
+    /* copy as much as possible from the source to the destination */
+    size_t copy_this_much = wt->sizeleft;
+    if(copy_this_much > buffer_size)
+      copy_this_much = buffer_size;
+    memcpy(dest, wt->readptr, copy_this_much);
+
+    wt->readptr += copy_this_much;
+    wt->sizeleft -= copy_this_much;
+    return copy_this_much; /* we copied this many bytes */
+  }
+
+  return 0; /* no more data left to deliver */
+}
+
+size_t Optional::OAuth::writefunc(void *ptr, size_t size, size_t nmemb, struct weird_string *s)
+{
+  size_t new_len = s->len + size*nmemb;
+  s->ptr = static_cast<char*>(realloc(s->ptr, new_len+1));
+  if (s->ptr == NULL) {
+    fprintf(stderr, "realloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  memcpy(s->ptr+s->len, ptr, size*nmemb);
+  s->ptr[new_len] = '\0';
+  s->len = new_len;
+
+  return size*nmemb;
 }
--- tests/oauth_tests.cpp
+++ tests/oauth_tests.cpp
@@ -20,24 +20,47 @@ TEST(OAuthTests, generate_authentication_url_composes_correct_string) {
     Optional::OAuth oauth("user_id", "https://callback_uri.test");
     const string expected_url = "https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=https://callback_uri.test&client_id=user_id@AMER.OAUTHAP";
     
-    EXPECT_STREQ(expected_url, oauth.generate_authentication_url());
+    EXPECT_STREQ(expected_url.c_str(), oauth.generate_authentication_url().c_str());
 }
     
 TEST(OAuthTests, oauth_handles_bad_uid_and_url) {
     Optional::OAuth oauth("", "");
     const string expected_url = "https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=https://null_value&client_id=null_value@AMER.OAUTHAP";
 
-    EXPECT_STREQ(expected_url, oauth.generate_authentication_url());
+    EXPECT_STREQ(expected_url.c_str(), oauth.generate_authentication_url().c_str());
 }
     
 TEST(OAuthTests, accept_authentication_code_accepts_good_code) {
     Optional::OAuth oauth("user_id", "https://callback_uri.test");
-    
+
     const string provided_code = "http://callback_uri.test/?code=<THIS_IS_THE_RETURNED_AUTH_CODE>";
     const string expected_code = "<THIS_IS_THE_RETURNED_AUTH_CODE>";
-    
+
     EXPECT_TRUE(oauth.accept_authentication_code(provided_code));
-    EXPECT_STREQ(expected_code, oauth.get_authentication_code());
+    EXPECT_STREQ(expected_code.c_str(), oauth.get_authentication_code().c_str());
 }
+
+TEST(OAuthTests, accept_authentication_code_handles_bad_code) {
+    Optional::OAuth oauth("user_id", "https://callback_uri.test");
+
+    const string BAD_provided_code = "ht:/callbacri.23@#$@^<THIS_IS_THE_RETURNED_AUTH_CODE>";
+    const string expected_code = "";
+
+    EXPECT_FALSE(oauth.accept_authentication_code(BAD_provided_code));
+    EXPECT_STREQ(expected_code.c_str(), oauth.get_authentication_code().c_str());
+}
+
+// There's no way to test this without legitimate credentials, so it's left as
+// a shell test for an end user to check.
+TEST(OAuthTests, /*DISABLED_*/refresh_code_is_retrieved_from_server) {
+    Optional::OAuth oauth("TEUX0001", "https://localhost");
+
+    const string provided_code = "https://localhost/?code=wjMTgrX5t1x0mr6b9GnvU2z36YhY5v6lbkmrCYPwuOWBGZPY9IZQ7K434s0EXFhILg75nahDhm77Ybfn4H%2BUuMRxoEXNnF02hoR6%2BgO%2BNd6DMIZ2dHJrgrHQjO6u7NbBa9iAQy8jzPHV4ieuAcCddhjC9iMSRLNX0N3Y88nIswdyT8Gdcb%2B30vILd4Eh0Uf%2BCgCxWDkbczWxLvoI3aNLLogIdXsAsN7TYf%2B7OU6L1SWqwOFW1rpUJiZcn2B7VWBESAf2SJB9uBZuomjc2Kieuz5o7YQB%2BPUbE7bs98TyGyuCe%2FjjMyQdHgZdskyqKOmw1Q2jbFcMA9nsg2T3hpEroW9ZYAHQGZzwMW6LU6dRhnAV84RnAc3ypZJHkMfzETA%2Bz5gXSY6gfcTMLoTa3YT9i%2FNIb1WP70AJVZr4nvsSHcKhivAN0CeTH7nmcd3100MQuG4LYrgoVi%2FJHHvltQYqeXE5JedsnBJKezV%2Ftaj7OAovFah90CL0QwvHoopeLvsTNNpQrr5xJqrg1%2BrrlGxa7Eb9nPYcgsp4rJpuhF%2Bhd8Iv%2FHLnHmgwJk%2Fappf16ydFMrGXsxGDRkDMEZ5IjQVY%2Bx6o1PXbKWL6ffi5CJThRe%2FS9%2BCbV3tWJImx5CUHuSwbhdXvpHAagU%2BsUEr3bG8YdiPu4q9sGmGoRHqmYgwHiS73od%2BMo5hhcIYIRzxjzOBQT6On0dNFk0BgJ%2F4GHXJJLoXskaDsduz870KQd5mo%2BBgkulH60UulglqAsy3CS3e%2Fplf5eflDnLMsC3afFqJ9GZd1UQ2kY1jYN3%2FbTyaAqa%2FesIeipOiRVFUV3Le5jkrMJjLonk2pJhjuXwdjpbLB%2BnnNaZ7OopMDKfolciR4RFopnWB356LNsxMzp%2FozxHQjwUtN2I5Ohgo%3D212FD3x19z9sWBHDJACbC00B75E";
+
+    EXPECT_TRUE(oauth.accept_authentication_code(provided_code));
+    EXPECT_STREQ("", oauth.generate_refresh_token().c_str());
+}
+
+
 
 } // namespace
