a10a16e5d8f2f136f69402c64ddfd1f7c46a0c1a
Tegan Lamoureux <tegan@teux.me>
2019-03-05 13:30:54 -0800

    Added field access through account object.

--- src/backend/account.cpp
+++ src/backend/account.cpp
@@ -1,6 +1,5 @@
 #include "account.h"
 
-#include "rapidjson/document.h"
 
 Optional::Account::Account(OAuth& authorization_in, Rest& rest_interface_in, std::string account_number_in)
     : authorization(authorization_in)
@@ -16,15 +15,92 @@ bool Optional::Account::refresh_account() {
     if (this->authorization.get_status() == Valid) {
         // Can also add 'fields=positions,orders' for more info.
         std::string header = std::string("Authorization: Bearer ") + authorization.get_access_token();
-
         std::string response = this->rest_interface.get(this->account_post_resource_url, header, success, true);
-
-        rapidjson::Document account_details;
-        account_details.Parse(response.c_str(), response.length());
-
-        // it works!!
+        this->account_details.Parse(response.c_str(), response.length());
     }
 
     return success;
+}
+
+// FIXME: refactor this out into a "get nested data" method. this is too much duplication.
+std::string Optional::Account::account_type() {
+    std::string data = "";
+
+    if(this->account_details.HasMember("securitiesAccount")) {
+        rapidjson::Value& account = this->account_details["securitiesAccount"];
+        if (account.HasMember("type")) {
+            data = account["type"].GetString();
+        }
+    }
+
+    return data;
+}
+
+std::string Optional::Account::account_id() {
+    std::string data = "";
+
+    if(this->account_details.HasMember("securitiesAccount")) {
+        rapidjson::Value& account = this->account_details["securitiesAccount"];
+        if (account.HasMember("accountId")) {
+            data = account["accountId"].GetString();
+        }
+    }
+
+    return data;
+}
+
+std::string Optional::Account::round_trips() {
+    std::string data = "";
+
+    if(this->account_details.HasMember("securitiesAccount")) {
+        rapidjson::Value& account = this->account_details["securitiesAccount"];
+        if (account.HasMember("roundTrips")) {
+            data = account["roundTrips"].GetString();
+        }
+    }
+
+    return data;
+}
+
+std::string Optional::Account::is_day_trader() {
+    std::string data = "";
+
+    if(this->account_details.HasMember("securitiesAccount")) {
+        rapidjson::Value& account = this->account_details["securitiesAccount"];
+        if (account.HasMember("isDayTrader")) {
+            data = account["isDayTrader"].GetString();
+        }
+    }
+
+    return data;
+}
+
+std::string Optional::Account::is_closing_only_restricted() {
+    std::string data = "";
+
+    if(this->account_details.HasMember("securitiesAccount")) {
+        rapidjson::Value& account = this->account_details["securitiesAccount"];
+        if (account.HasMember("isClosingOnlyRestricted")) {
+            data = account["isClosingOnlyRestricted"].GetString();
+        }
+    }
+
+    return data;
+}
+
+double Optional::Account::available_funds() {
+    double data = 0.0;
+
+    if(this->account_details.HasMember("securitiesAccount")) {
+        rapidjson::Value& account = this->account_details["securitiesAccount"];
+        if (account.HasMember("initialBalances")) {
+            rapidjson::Value& balances = account["initialBalances"];
+            if (account.HasMember("availableFundsNonMarginableTrade")) {
+                data = account["availableFundsNonMarginableTrade"].GetDouble();
+            }
+        }
+    }
+
+    return data;
 }
 
--- src/backend/account.h
+++ src/backend/account.h
@@ -4,6 +4,8 @@
 #include "oauth.h"
 #include "rest.h"
 
+#include "rapidjson/document.h"
+
 namespace Optional {
 
 // FIXME: This can be the top level object, which contains the rest api and the oauth object. The ueer will supply
@@ -17,6 +19,17 @@ public:
 
     bool refresh_account();
 
+    // FIXME: populate with rest of available fields. genericize access so that
+    //        each only uses the required string and substring data name, and json
+    //        parsing is offloaded to one method (as it should be). can make recursive?
+    //        pass in a n-length list of subterms and recurse until field found?
+    std::string account_type();
+    std::string account_id();
+    std::string round_trips();
+    std::string is_day_trader();
+    std::string is_closing_only_restricted();
+    double available_funds();
+
 private:
     Account() = delete;
 
@@ -25,6 +38,8 @@ private:
 
     std::string account_number;
     std::string account_post_resource_url = "https://api.tdameritrade.com/v1/accounts/";
+
+    rapidjson::Document account_details;
 };
 
 }
